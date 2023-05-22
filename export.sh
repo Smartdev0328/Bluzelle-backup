fccea64dca2af200da5a0becfa5bf2da7f973df8
smartdev0328 <smartdev0328@gmail.com>
2023-05-22 00:55:01 -0400

    fix: export_cmd

--- export.sh
+++ export.sh
@@ -1,3 +1,3 @@
 ssh -i alvi.pem user@host_ip '(docker exec -it <docker_container> sh -c "pkill curiumd")'
-ssh -i alvi.pem user@host_ip '(docker exec -it <docker_container> sh -c "curiumd export")' > exported_genesis.json
+ssh -i alvi.pem user@host_ip '(docker exec -it <docker_container> sh -c "curiumd export --for-zero-height --jail-allowed-addrs bluzellevaloper1syeqq3hl327d53w6wz9fzamuhklg4qk47rvcqd")' > exported_genesis.json
 ssh -i alvi.pem user@host_ip '(docker exec -it <docker_container> sh -c "curiumd start")'
